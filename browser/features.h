e61c1fc79b50a263ba729b8720ef8dc86d9abd58
Anton Lazarev <antonok35@gmail.com>
2019-11-21 16:54:30 -0800

    add brave://flags feature to enable cosmetic filtering

---
+++ browser/features.h
@@ -0,0 +1,17 @@
+/* Copyright 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_BROWSER_FEATURES_H_
+#define BRAVE_BROWSER_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace features {
+
+extern const base::Feature kBraveAdblockCosmeticFiltering;
+
+}   // namespace features
+
+#endif  // BRAVE_BROWSER_FEATURES_H_
